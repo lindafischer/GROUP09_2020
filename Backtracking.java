c90d3b5b2303a929d2cca94afaad3d1eea97c8e1
Elena Perego <72268824+elenaperego@users.noreply.github.com>
2020-10-04 17:14:53 +0200

    Update Backtracking.java

--- Backtracking.java
+++ Backtracking.java
@@ -1,3 +1,16 @@
 public class Backtracking {
+  public static void main (String[] args) {
+    int n = 4;
+    int m = 6;
+    int [][] adj = {{0,1,1,1},{1,0,1,1},{1,1,0,1},{1,1,1,0}};
+    int [] x = new int [n];
+  }
   
+  public static int [] GraphColor (int [][] adj, int n, int m, int [] x) {
+    for (int node = 1; node <= n; node++) {
+      for (int color = 1; color <= n; color++) {
+        if (isValid(node, color)) {
+          x [node] = color;
+        }
+        if (
 }
