f760dd00ec8349a10fa045cb6722ca1f89767cef
pariidanDKE <72300855+pariidanDKE@users.noreply.github.com>
2020-10-04 20:42:59 +0200

    Update Backtracking.java

--- Backtracking.java
+++ Backtracking.java
@@ -15,3 +15,28 @@ public class Backtracking {
         if ((node + 1) < n)
           GraphColor(
 }
+      	int [][] adj_matrix= new int[n+1][n+1];
+			
+			
+			for(int i=0;i<m;i++){
+				
+				int x=e[i].u;
+				int y=e[i].v;
+				System.out.println("X : "+ x);
+				System.out.println("Y : "+ y);
+				adj_matrix[x][y]=1;
+				adj_matrix[y][x]=1;
+				
+			}	
+				
+					
+			
+			
+			
+			
+			for(int i=1;i<n+1;i++){
+				for(int j=1;j<n+1;j++){
+					System.out.print(adj_matrix[i][j]+ " ");
+				}
+				System.out.println();
+			}
