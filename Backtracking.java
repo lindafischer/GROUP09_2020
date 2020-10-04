7fbd95df05c8a35c753157f2b30ffda81d11dab2
Elena Perego <72268824+elenaperego@users.noreply.github.com>
2020-10-04 22:04:49 +0200

    Update Backtracking.java

--- Backtracking.java
+++ Backtracking.java
@@ -1,42 +1,48 @@
 public class Backtracking {
   public static void main (String[] args) {
-    int n = 4;
-    int m = 6;
-    int [][] adj = {{0,1,1,1},{1,0,1,1},{1,1,0,1},{1,1,1,0}};
-    int [] x = new int [n];
-  }
-  
-  public static int [] GraphColor (int [][] adj, int n, int m, int [] x) {
-    for (int node = 1; node <= n; node++) {
-      for (int color = 1; color <= n; color++) {
-        if (isValid(node, color)) {
-          x [node] = color;
-        }
-        if ((node + 1) < n)
-          GraphColor(
+  	int [][] adj_matrix= new int[n+1][n+1];
+	
+	for(int i=0;i<m;i++){
+		int x=e[i].u;
+		int y=e[i].v;
+		System.out.println("X : "+ x);
+		System.out.println("Y : "+ y);
+		adj_matrix[x][y]=1;
+		adj_matrix[y][x]=1;
+	}	
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<n+1;j++){
+			System.out.print(adj_matrix[i][j]+ " ");
+		}
+		System.out.println();
+	}
+	  
+	int [] colors = new int [n];
+	for (int k = 0; k <= n; k++) { // k = current index to test
+		int ChromaticNumber = GraphColoring(adj_matrix, k, colors, n)
+	}
+			
+public static int GraphColoring(int adj_matrix[][], int k, int colors[], int n) {
+	for (int c = 1; c <= m; c++) { 
+		if (isValid(adj_matrix, k, colors, n)) { 
+			colors[k] = c; 
+		}
+		if ((k+1) < n) {
+			GraphColoring(adj_matrix, k+1, colors, n);
+		}
+		else {
+		// length of the filled part of the array colors = chromatic number 
+		return ChromaticNumber;
+		}
+	}
+	return 0;
 }
-      	int [][] adj_matrix= new int[n+1][n+1];
 			
-			
-			for(int i=0;i<m;i++){
-				
-				int x=e[i].u;
-				int y=e[i].v;
-				System.out.println("X : "+ x);
-				System.out.println("Y : "+ y);
-				adj_matrix[x][y]=1;
-				adj_matrix[y][x]=1;
-				
-			}	
-				
-					
-			
-			
-			
-			
-			for(int i=1;i<n+1;i++){
-				for(int j=1;j<n+1;j++){
-					System.out.print(adj_matrix[i][j]+ " ");
-				}
-				System.out.println();
-			}
+public static boolean isValid (int adj_matrix[][], int k, colors[], int n) {
+	for (int i = 0; i < n; i++) {
+		if (adj_matrix[k][i] == 1 && c == x[i]) {
+			return false;
+		}
+	}
+	return true;
+}
