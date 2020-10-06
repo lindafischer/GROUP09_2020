4549283afca9a0b3f5cdc30d2b6de1fb106bd9c4
SimonKoehl01 <72283444+SimonKoehl01@users.noreply.github.com>
2020-10-06 22:26:16 +0200

    Update BruteForce.java

--- BruteForce.java
+++ BruteForce.java
@@ -1,17 +1,26 @@
 public class BruteForce {
+  public static long startTime;
   public static int ExactChromNumber(int n,int [] colors,int[][] adj_matrix){
+    startTime = System.nanoTime();
     for(int i = 1; i <= n; i++) { // colors from 1 to n
-      if(Colorings(1,i,colors,n,adj_matrix)) { // if graph can be colored using i colors starting at vertex 0
+      int temp = Colorings(1,i,colors,n,adj_matrix);
+      if(temp == 1) { // if graph can be colored using i colors starting at vertex 0
         return i;
+      }
+      else if(temp == -1) {
+        return -1;
       }
     }
     return 1; // program will never get to this line
   }
   //colors graph using m colors starting at vertex v
-  public static boolean Colorings(int v, int m,int [] colors,int n,int [][]adj_matrix) {
-    HelperFunctions HF=new HelperFunctions();
+  public static int Colorings(int v, int m,int [] colors,int n,int [][]adj_matrix) {
+    double elapsedTime = (System.nanoTime() - startTime) / 1000000000;
+    if(elapsedTime > 30) {
+      return -1;
+    }
     if(v > n-1) { //if all vertices have been colored
-      return true;
+      return 1;
     }
     else {
       for(int i = 1; i <= m; i++) {
@@ -26,13 +35,13 @@ public class BruteForce {
         }
 
         if(match == false) { // if they are not adjacient
-          if(Colorings(v+1,m,colors,n,adj_matrix)) // use recursion for the next vertex
-          return true;
+          if(Colorings(v+1,m,colors,n,adj_matrix) == 1) // use recursion for the next vertex
+          return 1;
 
         }
       }
       colors[v-1] = 0; //if v is close to another vertex with the same color, assign color 0 to vertex v and fail
-      return false;
+      return 0;
     }
   }
 }
