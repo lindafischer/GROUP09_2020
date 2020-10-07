03f0b02dc88439eee30b3637da70efa048bf2f88
Elena Perego <72268824+elenaperego@users.noreply.github.com>
2020-10-07 15:50:10 +0200

    Update BruteForce.java

--- BruteForce.java
+++ BruteForce.java
@@ -20,7 +20,7 @@ public class BruteForce {
         return -1;
       }
     }
-    return 1; // program will never get to this line
+    return 1; // program will never get to this line because it will never use less than 1 color
   }
 
   /**
@@ -37,22 +37,22 @@ public class BruteForce {
     if(elapsedTime > timeLimit) {
       return -1;
     }
-    if(v > n-1) { //if all vertices have been colored
+    if(v > n-1) { // check if all vertices have been colored
       return 1;
     }
     else {
       for(int i = 1; i <= m; i++) {
-        boolean match = false;
-        colors[v-1] = i; //assign color i to vertex v
+        boolean valid = false;
+        colors[v-1] = i; // try to assign color i to the current vertex v
 
         for(int j = 1; j < n; j++) {
-          if(adj_matrix[v-1][j-1] == 1) { // if the current vertex is adjacient with all other vertices
+          if(adj_matrix[v-1][j-1] == 1) { // check if the current vertex v is adjacient with all the other vertices
             if(i == colors[j-1]) // if it is, check if they have the same color
-            match = true;
+            valid = true;
           }
         }
 
-        if(match == false) { // if they are not adjacient
+        if(valid == false) { // if they are not adjacient and they do not have the same color
           if(Colorings(v+1,m,colors,n,adj_matrix, timeLimit) == 1) // use recursion for the next vertex
           return 1;
 
