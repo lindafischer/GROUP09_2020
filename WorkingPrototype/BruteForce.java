dd591dabc518efc8928f33b45c1035c9c10ad5b7
SimonKoehl01 <72283444+SimonKoehl01@users.noreply.github.com>
2020-10-08 15:37:19 +0200

    Add files via upload

---
+++ WorkingPrototype/BruteForce.java
@@ -0,0 +1,66 @@
+public class BruteForce {
+  public static long startTime;
+
+  /**
+  This function tries to compute the exact chromatic number by brute forcing all possible colorings.
+  @param n Number of vertices
+  @param adj_matrix The adjacency matrix
+  @param timeLimit The wanted timeLimit which should not be exceeded
+  @return The exact chromatic number or -1 if the brute forcing attempt took more than 30 seconds.
+  */
+  public static int ExactChromNumber(int n, int[][] adj_matrix, int timeLimit){
+    int[] colors = new int[n];
+    startTime = System.nanoTime();
+    for(int i = 1; i <= n; i++) { // colors from 1 to n
+      int temp = Colorings(1,i,colors,n,adj_matrix, timeLimit);
+      if(temp == 1) { // if graph can be colored using i colors starting at vertex 0
+        return i;
+      }
+      else if(temp == -1) {
+        return -1;
+      }
+    }
+    return 1; // program will never get to this line
+  }
+
+  /**
+  This function colors the graph using m colors starting at vertex v
+  @param v int, the current vertex
+  @param m The number of colors
+  @param n The number of vertices
+  @param adj_matrix The adjacency matrix
+  @param timeLimit The wanted timeLimit which should not be exceeded
+  @return 1 if a coloring is possible, 0 if not and -1 if the time limit is exceeded
+  */
+  public static int Colorings(int v, int m, int[] colors,int n,int[][] adj_matrix, int timeLimit) {
+    double elapsedTime = (System.nanoTime() - startTime) / 1000000000.0;
+    if(elapsedTime > timeLimit) {
+      return -1;
+    }
+    if(v > n-1) { //if all vertices have been colored
+      return 1;
+    }
+    else {
+      for(int i = 1; i <= m; i++) {
+        boolean match = false;
+        colors[v-1] = i; //assign color i to vertex v
+
+        for(int j = 1; j < n; j++) {
+          if(adj_matrix[v-1][j-1] == 1) { // if the current vertex is adjacient with all other vertices
+            if(i == colors[j-1]) {// if it is, check if they have the same color
+              match = true;
+            }
+          }
+        }
+
+        if(match == false) { // if they are not adjacient
+          if(Colorings(v+1,m,colors,n,adj_matrix, timeLimit) == 1) // use recursion for the next vertex
+          return 1;
+
+        }
+      }
+      colors[v-1] = 0; //if v is close to another vertex with the same color, assign color 0 to vertex v and fail
+      return 0;
+    }
+  }
+}
---
+++ WorkingPrototype/HelperFunctions.java
@@ -0,0 +1,149 @@
+public class HelperFunctions {
+  /**
+  This function returns the adjacency matrix for a given graph (1 if two nodes
+  are connected, 0 if not)
+  @param e <i>List of ColEdge objects</i>
+  @param m Number of edges in the graph
+  @param n Number of vertices in the graph
+  @return Adjacency Matrix
+  */
+  public static int[][] getAdjacencyMatrix(ColEdge[] e, int m, int n) {
+    int [][] adj_matrix= new int[n][n];
+    for(int i = 0; i < m; i++){
+      int x=e[i].u;
+      int y=e[i].v;
+      adj_matrix[x-1][y-1]=1;
+      adj_matrix[y-1][x-1]=1;
+    }
+    return adj_matrix;
+  }
+
+  /**
+  This function returns a list of degrees for each node, where the index is the
+  corresponding node/vertex
+  @param a The adjacency matrix, represented by a 2D-array, can be computed using the <i>getAdjacencyMatrix</i> method
+  @return List of degrees
+  */
+  public static int[] getDegrees(int[][] a) {
+    int[] degrees = new int[a.length];
+    for(int i = 0; i < a.length; i++) {
+      for(int j = 0; j < a.length; j++) {
+        if(a[j][i] == 1) {
+          degrees[i]++;
+        }
+      }
+    }
+    return degrees;
+  }
+
+  /**
+  Custom max function for any 1D-array of arbitrary length
+  @param arr <i>Array to compute the max of</i>
+  @return Maximum int value for the given array
+  */
+  public static int getMax(int[] arr) {
+    int record = 0;
+    for(int i = 0; i < arr.length; i++) {
+      if(arr[i] > record) {
+        record = arr[i];
+      }
+    }
+    return record;
+  }
+
+  /**
+  This functions prepares the given array for a 2-dimensional sorting
+  (Needed to not loose track of which node has the highest degree when sorting by degree)
+  <h2>Important Note</h2>
+  Please use this method to sort your arrays while preserving the indices, not recursive2DSortHelper.
+  (See explanation under recursive2DSortHelper)
+  @param arr The array to prepare
+  @return The prepared 2D array
+  */
+  public static int[][] TwoDSort(int[] arr) {
+    int[][] result = new int[arr.length][2];
+    for(int i = 0; i < arr.length; i++) {
+      result[i][0] = arr[i];
+      result[i][1] = i;
+    }
+    return recursiveTwoDSortHelper(result);
+  }
+
+  /**
+  This function sorts any 1D array while preserving the indices
+  <h2>Important Note</h2>
+  This function is called within TwoDSort and therefore is just a helper function to the before mentioned,
+  to sort your array please use TwoDSort instead.
+  @param arr The array to sort
+  @return A sorted 2D array with arr[i][0] being the value and arr[i][1] the original index
+  */
+  public static int[][] recursiveTwoDSortHelper(int[][] arr) {
+    int sortedCount = 0;
+    for(int i = 1; i < arr.length; i++) {
+      if(arr[i-1][0] > arr[i][0]) {
+        int temp0 = arr[i-1][0];
+        int temp1 = arr[i-1][1];
+        arr[i-1][0] = arr[i][0];
+        arr[i-1][1] = arr[i][1];
+        arr[i][0] = temp0;
+        arr[i][1] = temp1;
+      }
+      else {
+        sortedCount++;
+      }
+    }
+    if(sortedCount == arr.length-1) {
+      return arr;
+    }
+    return recursiveTwoDSortHelper(arr);
+  }
+
+  /**
+  This function returns the trivial upper bound
+  @param arr Adjacency Matrix for a graph
+  @return Trivial upper bound (MaxDegree + 1)
+  */
+  public static int getTrivialUpperBound(int[][] arr) {
+    int[] deg = getDegrees(arr);
+    return getMax(deg) + 1;
+  }
+
+  /**
+  This function returns the most trivial lower bound (1 if the graph has no edges, 2 otherwise)
+  @param m Number of edges
+  @return Trivial lower bound
+  */
+  public static int getTrivialLowerBound(int m) {
+    if(m == 0) return 1;
+    else return 2;
+  }
+
+  /**
+  This function returns the index of the maximum value in the array
+  @param arr Array to find the maxIndex in
+  @return Index of the maximum value
+  */
+  public static int getMaxIndex(int[] arr) {
+    int maxVal = getMax(arr);
+    for(int i = 0; i < arr.length; i++) {
+      if(arr[i] == maxVal) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static int getLowerBoundsByTriangleDetection(ColEdge[] e) {
+    TriangleDetection triangleDetector = new TriangleDetection();
+    return triangleDetector.run(e);
+  }
+
+  public static int checkIfComplete(int m, int n) {
+    if((n * (n-1) / 2) == m) {
+      return n;
+    }
+    else {
+      return -1;
+    }
+  }
+}
