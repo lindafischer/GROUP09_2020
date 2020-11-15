adf074392f1dfc4e88bfd3b251af563266b2a395
Elena Perego <72268824+elenaperego@users.noreply.github.com>
2020-11-15 18:47:15 +0100

    Update Menu-JavaFX.java

--- Menu-JavaFX.java
+++ Menu-JavaFX.java
@@ -40,18 +40,18 @@ public class MenuProject extends Application
 	{
 		//Page size
 		Pane root = new Pane();
-    root.setPrefSize(1050, 600);
+    		root.setPrefSize(1050, 600);
         
 		//Set background
 		Image background = new Image("sfondo1.jpg");
 		ImageView backgroundView = new ImageView(background);
 		backgroundView.setFitWidth(1050);
-    backgroundView.setFitHeight(600);
+    		backgroundView.setFitHeight(600);
 		
 		//Title of the game
 		Text title = new Text ("GRAPH COLORING GAME");
-    title.setTranslateX(200);
-    title.setTranslateY(150);
+    		title.setTranslateX(200);
+    		title.setTranslateY(150);
 		title.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
 		title.setFill(Color.WHITE);
 		title.setStrokeWidth(2);
@@ -60,9 +60,9 @@ public class MenuProject extends Application
 		
 		//Game Mode 1
 		Rectangle rectangle1 = new Rectangle(300,80);
-    rectangle1.setOpacity(0.4); 
+    		rectangle1.setOpacity(0.4); 
 		rectangle1.setX(20);
-    rectangle1.setY(290);
+    		rectangle1.setY(290);
 		Text mode1 = new Text("Game Mode 1");
 		mode1.setFill(Color.WHITE);
 		mode1.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 40));
@@ -74,9 +74,9 @@ public class MenuProject extends Application
 		
 		//Game Mode 2
 		Rectangle rectangle2 = new Rectangle(300,80);
-    rectangle2.setOpacity(0.4); 
+    		rectangle2.setOpacity(0.4); 
 		rectangle2.setX(370);
-    rectangle2.setY(290);
+    		rectangle2.setY(290);
 		Text mode2 = new Text("Game Mode 2");
 		mode2.setFill(Color.WHITE);
 		mode2.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 40));
@@ -88,9 +88,9 @@ public class MenuProject extends Application
 		
 		//Game Mode 3
 		Rectangle rectangle3 = new Rectangle(300,80);
-    rectangle3.setOpacity(0.4); 
+    		rectangle3.setOpacity(0.4); 
 		rectangle3.setX(720);
-    rectangle3.setY(290);
+    		rectangle3.setY(290);
 		Text mode3 = new Text("Game Mode 3");
 		mode3.setFill(Color.WHITE);
 		mode3.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 40));
