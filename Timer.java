38234de022433f4b7cd55f291bba90a772a4c080
Anguio <72300846+Anguio@users.noreply.github.com>
2020-11-17 09:26:12 +0100

    Create Timer.java

---
+++ Timer.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class Timer {
+
+    private Date startDate;
+    private Long time;
+
+    Timer() {
+
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public Long getTimeInSeconds() {
+        return time / 1000;
+    }
+
+    public Long getTimeInNanoSeconds() {
+        return time * 1000000;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public void start() {
+        this.startDate = new Date();
+    }
+
+    public Long end() {
+        this.time = startDate.getTime() - new Date().getTime();
+        return this.time;
+    }
+
+}
