12eda8ba0a839c104024968154179db1400443ea
SimonKoehl01 <72283444+SimonKoehl01@users.noreply.github.com>
2020-12-08 15:05:20 +0100

    Update ColorButton.java

--- Commented Files/ColorButton.java
+++ Commented Files/ColorButton.java
@@ -1,10 +1,14 @@
 import javafx.scene.control.Button;
 
-/*The color button is an extension for the button 
-* This class is used to provide the Color Button Objects 
-* In order to color the nodes 
-*/
 public class ColorButton extends Button {
+  /*
+  * The ColorButton extends the JavaFX Button class and 
+  * is used to provide the ColorButton objects grouped in the ColorPalette
+  * in order to color the nodes
+  * @param c A String containing the Buttons color as a CSS color value
+  * @param xPos The x-position to draw the ColorButton at
+  * @param yPos The y-position to draw the ColorButton at
+  */
   public ColorButton(String c, int xPos, int yPos) {
     super("");
     setLayoutX(xPos);
@@ -12,12 +16,16 @@ public class ColorButton extends Button {
     setStyle(color + c + ";");
     stringColor = c;
   }
-
+  
+  /**
+  * This method is used to get the ColorButton's color
+  * @return A String containing the ColorButton's color as a CSS color value
+  */
   public String getColor() {
     return stringColor;
   }
-  private String stringColor;
   
-  //This is to color the background of the color button
+  private String stringColor;
+  //This is to color the background of the color button (the CSS color value will be appended to this)
   private String color = "-fx-background-color: ";
 }
