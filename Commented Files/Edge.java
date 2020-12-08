49eeb4b8f1bc5ae5fbfa51b1cf8de5f4641995d2
SimonKoehl01 <72283444+SimonKoehl01@users.noreply.github.com>
2020-12-08 12:45:26 +0100

    Documented versions

---
+++ Commented Files/Edge.java
@@ -0,0 +1,9 @@
+//This class is presented as a Line and is called edge to make it more understandable 
+//As you can see below this class extends Line
+
+import javafx.scene.shape.Line;
+public class Edge extends Line {
+  public Edge(double x1, double y1, double x2, double y2) {
+    super(x1, y1, x2, y2);
+  }
+}
